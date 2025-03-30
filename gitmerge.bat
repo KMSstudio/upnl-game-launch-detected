91e5bdf0566eb1fae8aca1943c25c79e3505e5ec
MS Kang <tomskang@naver.com>
2025-03-31 02:38:40 +0900

    update 22

--- gitmerge.bat
+++ gitmerge.bat
@@ -17,17 +17,17 @@ set /p ver="Version name: "
 git checkout develop
 
 :: Versioning (modifies package.json and package-lock.json)
-npm version !ver!
+npm version %ver%
 
 :: Stage and commit version bump
 git add package.json package-lock.json
-git commit -m "!ver!"
+git commit -m "%ver%"
 
 :: Move to main branch
 git checkout main
 
 :: Merge develop into main
-git merge develop -m "Merge version !ver! from develop"
+git merge develop -m "Merge version %ver% from develop"
 
 :: Push changes to main and tags
 git push origin main
@@ -37,5 +37,5 @@ git push origin --tags
 git checkout develop
 
 echo ================================
-echo Merged version !ver! into main
+echo Merged version %ver% into main
 echo ================================
