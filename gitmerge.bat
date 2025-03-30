a0b1107d8df4357452f0b7f312d87f0299c31618
MS Kang <tomskang@naver.com>
2025-03-31 02:21:04 +0900

    update gitmerge.bar

---
+++ gitmerge.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo ================================
+echo Recent git tag:
+echo -------------------------------
+for /f "tokens=*" %%A in ('git for-each-ref --sort=-creatordate --count=3 --format="%%(refname:short)" refs/tags') do (
+    echo %%A
+)
+echo ================================
+
+set /p msg="Merge commit message: "
+set /p ver="New version: "
+
+:: move to develop branch
+git checkout develop
+git pull origin develop
+
+:: move to main branch
+git checkout main
+git pull origin main
+
+:: merge develop into main
+git merge develop -m "%msg%"
+
+:: new version
+npm version %ver%
+
+:: push
+git push origin main --follow-tags
+git push --tags
+git checkout develop
